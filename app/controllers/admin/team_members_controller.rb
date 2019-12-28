98e8b80c95367ef5dc62eae4167af95d2306d3d1
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 19:19:34 +0000

    add admin team members controller

---
+++ app/controllers/admin/team_members_controller.rb
@@ -0,0 +1,23 @@
+class Admin::TeamMembersController < ApplicationController
+
+  # http_basic_authenticate_with name: ENV["WEBSITE_USERNAME"], password: ENV["WEBSITE_PASSWORD"]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
