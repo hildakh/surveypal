3c8efd658fb6f155ca44935cd5692ff9cc4fd42b
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 18:15:40 +0000

    add user controller with private placeholder fn for user params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  private
+
+    def user_params
+      # params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+end
