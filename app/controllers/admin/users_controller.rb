0f206f1621db2c54448e02619d37133b0cddfe5c
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 19:23:30 +0000

    add admin users controller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,35 @@
+class Admin::UsersController < ApplicationController
+
+  def index
+  end
+  
+  def new
+  end
+
+  def create
+    # Below from jungle user signup
+    # user = User.new(user_params)
+    # if user.save
+    #   session[:user_id] = user.id
+    #   redirect_to '/', notice: 'Account created successfully!'
+    # else
+    #   redirect_to '/signup'
+    # end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+
+  private
+
+    def user_params
+      # params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+end
