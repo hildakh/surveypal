166f3965d427fef11a5776878885626de0936521
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 18:32:44 +0000

    add controller admin dir, with dashboard controller

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class Admin::DashboardController < ApplicationController
+
+  # http_basic_authenticate_with name: ENV["WEBSITE_USERNAME"], password: ENV["WEBSITE_PASSWORD"]
+
+  def index
+  end
+
+end
