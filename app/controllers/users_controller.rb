05495af3a19723d38da110f4760433ea303caa57
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 18:18:39 +0000

    add placeholder user create fn. note: not needed at this point?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create  # Is create required at this point, since users cannot create their own new account?
+    # user = User.new(user_params)
+    # if user.save
+    #   session[:user_id] = user.id
+    #   redirect_to '/', notice: 'Account created successfully!'
+    # else
+    #   redirect_to '/signup'
+    # end
+  end
+
   private
 
     def user_params
