f89f7fd12af5b0b0fa91aaff3266aae3d887ce99
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 20:14:44 +0000

    remove methods from api users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,22 +1,3 @@
 class Api::UsersController < ApplicationController
 
-  def new
-  end
-
-  def create  # Is create required at this point, since users cannot create their own new account?
-    # user = User.new(user_params)
-    # if user.save
-    #   session[:user_id] = user.id
-    #   redirect_to '/', notice: 'Account created successfully!'
-    # else
-    #   redirect_to '/signup'
-    # end
-  end
-
-  private
-
-    def user_params
-      # params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
-
 end
