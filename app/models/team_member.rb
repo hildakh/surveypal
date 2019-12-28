94253ac0867aee8772ea839938801b4e849a4757
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-28 11:15:43 -0800

    add more relations to database

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,5 +1,5 @@
 class TeamMember < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   belongs_to :team
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   belongs_to :user_type
   has_many :team
-  # has_many :survey_question_answer
+  has_many :survey_question_answer
   has_many :survey
-  # has_many :team_member
+  has_many :team_member
 
   validates :name , presence: true
   validates :email , presence: true
