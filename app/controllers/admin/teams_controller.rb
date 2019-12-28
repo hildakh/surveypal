dba9ec549fb7f3ec09cdadac8147d0b9254da7ed
Vanessa Kroeker <vkroeker@gmail.com>
2019-12-28 18:41:43 +0000

    add teams controller

---
+++ app/controllers/admin/teams_controller.rb
@@ -0,0 +1,11 @@
+class Admin::TeamsController < ApplicationController
+
+  # http_basic_authenticate_with name: ENV["WEBSITE_USERNAME"], password: ENV["WEBSITE_PASSWORD"]
+
+  def index
+  end
+
+  def show
+  end
+
+end
