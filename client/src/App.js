2b87dfaf66ea37d5f45fd3e4fd5f899ce9805161
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2019-12-28 17:49:16 -0800

    initial layout for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     message: 'Click the button to load data!'
+  //   }
+  // }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data) // The entire response from the Rails API
+  // fetchData = () => {
+  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+  //     .then((response) => {
+  //       // handle success
+  //       console.log(response.data) // The entire response from the Rails API
 
-        console.log(response.data.message) // Just the message
-        this.setState({
-          message: response.data.message
-        });
-      })
-  }
+  //       console.log(response.data.message) // Just the message
+  //       this.setState({
+  //         message: response.data.message
+  //       });
+  //     })
+  // }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.message}</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+          <img src="./favicon.png" width="70" height="70" class="logo d-inline-block align-top" alt="logo" />
+          <a routerLink="/" class="navbar-brand app-name" href="#">SurveyPal</a>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <h6>
+                <a class="nav-link" href="contact">Login</a>
+              </h6>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
