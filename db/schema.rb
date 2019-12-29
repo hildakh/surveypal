ddd0e45972729198167265ae07d405bceb357ddf
hildakh <h_khajehamiri@yahoo.com>
2019-12-29 01:38:07 +0000

    bcrypt added to the list of gems. question model changed and dependency validation removed. survey table changed to include user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_220531) do
+ActiveRecord::Schema.define(version: 2019_12_29_012930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,9 +50,11 @@ ActiveRecord::Schema.define(version: 2019_12_28_220531) do
   create_table "surveys", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.datetime "end_date"
-    t.integer "created_by"
-    t.integer "city_id"
+    t.string "end_date"
+    t.string "date"
+    t.string "user_id"
+    t.string "integer"
+    t.string "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
