18646b0bfc8d78e6f913715705e6f626ce006913
hildakh <h_khajehamiri@yahoo.com>
2019-12-31 21:59:51 +0000

    new seeds added. question 1 was created as a question type to mark questions which are not dependent on other questions for response

--- db/seeds.rb
+++ db/seeds.rb
@@ -115,267 +115,279 @@ puts "Creating teams ..."
 Team.destroy_all
 
 teambahareh = Team.create!({
-  name: 'Team B',
+  name: 'Team Bahareh',
   description: 'Team created by Bahareh',
   purpose: 'To keep track of the surveyors and update them',
   user_id: 1
   })
       
 teamvanessa = Team.create!({
-  name: 'Team V',
+  name: 'Team Vanessa',
   description: 'Team created by Vanessa',
   purpose: 'To keep track of the surveyors and update them',
   user_id: 2
 })
 
 teamhilda = Team.create!({
-  name: 'Team H',
+  name: 'Team Hilda',
   description: 'Team created by Hilda',
   purpose: 'To keep track of the surveyors and update them',
   user_id: 3
 })
 
-## TEAM MEMBERS
-# puts "Creating team members ..."
+# TEAM MEMBERS
+puts "Creating team members ..."
 
-# TeamMember.destroy_all
+TeamMember.destroy_all
 
-# TeamMember.create!({
-#   team_id: 1,
-#   user_id: 4,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 1,
+  user_id: 4,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 1,
-#   user_id: 5,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 1,
+  user_id: 5,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 1,
-#   user_id: 6,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 1,
+  user_id: 6,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 2,
-#   user_id: 7,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 2,
+  user_id: 7,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 2,
-#   user_id: 8,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 2,
+  user_id: 8,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 2,
-#   user_id: 9,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 2,
+  user_id: 9,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 3,
-#   user_id: 10,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 3,
+  user_id: 10,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 3,
-#   user_id: 11,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 3,
+  user_id: 11,
+  active: true
+})
 
-# TeamMember.create!({
-#   team_id: 3,
-#   user_id: 12,
-#   active: true
-# })
+TeamMember.create!({
+  team_id: 3,
+  user_id: 12,
+  active: true
+})
 
-# ## QUESTIONS
-# puts "Creating questions ..."
+## QUESTIONS
+puts "Creating questions ..."
 
-# Question.destroy_all
+Question.destroy_all
 
-# Question.create!({
-#   description: 'Will you be sleeping in this shelter tonight?'
-# })
+Question.create!({
+  description: 'This question is independent from any other questions',
+  question_id: 1
+})
 
-# Question.create!({
-#   description: 'Have you already answered a survey today (with
-#   someone wearing a yellow sticker)?'
-# })
 
-# Question.create!({
-#   description: 'Have you already answered a survey in another
-#   community in the last 6 weeks?'
-# })
+Question.create!({
+  description: 'Will you be sleeping in this shelter tonight?',
+  question_id: 1
+})
 
-# Question.create!({
-#   description: 'Do you currently have a place to stay where you pay
-#   monthly rent?'
-# })
+Question.create!({
+  description: 'Have you already answered a survey today (with
+  someone wearing a yellow sticker)?',
+  question_id: 1
+})
 
-# Question.create!({
-#   description: 'How long have you been without a place of your own?'
-# })
+Question.create!({
+  description: 'Have you already answered a survey in another
+  community in the last 6 weeks?',
+  question_id: 1
+})
 
-# Question.create!({
-#   description: 'How old are you [OR] what year were you born?'
-# })
+Question.create!({
+  description: 'Do you currently have a place to stay where you pay
+  monthly rent?',
+  question_id: 1
+})
 
-# ## QUESTION OPTIONS
-# puts "Creating question options ..."
+Question.create!({
+  description: 'How long have you been without a place of your own?',
+  question_id: 1
+})
 
-# QuestionOption.destroy_all
+Question.create!({
+  description: 'How old are you [OR] what year were you born?',
+  question_id: 1
+})
 
-# QuestionOption.create!({
-#   question_id: 1,
-#   option_text: 'Yes',
-#   serial_order: 1
-# })
+## QUESTION OPTIONS
+puts "Creating question options ..."
 
-# QuestionOption.create!({
-#   question_id: 1,
-#   option_text: 'No',
-#   serial_order: 2
-# })
+QuestionOption.destroy_all
 
-# QuestionOption.create!({
-#   question_id: 1,
-#   option_text: 'No Answer',
-#   serial_order: 3
-# })
+QuestionOption.create!({
+  question_id: 1,
+  option_text: 'Yes',
+  serial_order: 1
+})
 
-# QuestionOption.create!({
-#   question_id: 2,
-#   option_text: 'Yes',
-#   serial_order: 1
-# })
+QuestionOption.create!({
+  question_id: 1,
+  option_text: 'No',
+  serial_order: 2
+})
 
-# QuestionOption.create!({
-#   question_id: 2,
-#   option_text: 'No',
-#   serial_order: 2
-# })
+QuestionOption.create!({
+  question_id: 1,
+  option_text: 'No Answer',
+  serial_order: 3
+})
 
-# QuestionOption.create!({
-#   question_id: 2,
-#   option_text: 'No Answer',
-#   serial_order: 3
-# })
+QuestionOption.create!({
+  question_id: 2,
+  option_text: 'Yes',
+  serial_order: 1
+})
 
-# QuestionOption.create!({
-#   question_id: 3,
-#   option_text: 'Yes',
-#   serial_order: 1
-# })
+QuestionOption.create!({
+  question_id: 2,
+  option_text: 'No',
+  serial_order: 2
+})
 
-# QuestionOption.create!({
-#   question_id: 3,
-#   option_text: 'No',
-#   serial_order: 2
-# })
+QuestionOption.create!({
+  question_id: 2,
+  option_text: 'No Answer',
+  serial_order: 3
+})
 
-# QuestionOption.create!({
-#   question_id: 3,
-#   option_text: 'Don''t know / No Answer',
-#   serial_order: 3
-# })
+QuestionOption.create!({
+  question_id: 3,
+  option_text: 'Yes',
+  serial_order: 1
+})
 
-# QuestionOption.create!({
-#   question_id: 4,
-#   option_text: 'Yes',
-#   serial_order: 1
-# })
+QuestionOption.create!({
+  question_id: 3,
+  option_text: 'No',
+  serial_order: 2
+})
 
-# QuestionOption.create!({
-#   question_id: 4,
-#   option_text: 'No',
-#   serial_order: 2
-# })
+QuestionOption.create!({
+  question_id: 3,
+  option_text: 'Don''t know / No Answer',
+  serial_order: 3
+})
 
-# QuestionOption.create!({
-#   question_id: 4,
-#   option_text: 'No Answer',
-#   serial_order: 3
-# })
+QuestionOption.create!({
+  question_id: 4,
+  option_text: 'Yes',
+  serial_order: 1
+})
 
-# QuestionOption.create!({
-#   question_id: 5,
-#   option_text: 'Days',
-#   serial_order: 1
-# })
+QuestionOption.create!({
+  question_id: 4,
+  option_text: 'No',
+  serial_order: 2
+})
 
-# QuestionOption.create!({
-#   question_id: 5,
-#   option_text: 'Weeks',
-#   serial_order: 2
-# })
+QuestionOption.create!({
+  question_id: 4,
+  option_text: 'No Answer',
+  serial_order: 3
+})
 
-# QuestionOption.create!({
-#   question_id: 5,
-#   option_text: 'Months',
-#   serial_order: 3
-# })
+QuestionOption.create!({
+  question_id: 5,
+  option_text: 'Days',
+  serial_order: 1
+})
 
-# QuestionOption.create!({
-#   question_id: 5,
-#   option_text: 'Years',
-#   serial_order: 4
-# })
+QuestionOption.create!({
+  question_id: 5,
+  option_text: 'Weeks',
+  serial_order: 2
+})
 
-# QuestionOption.create!({
-#   question_id: 5,
-#   option_text: 'No Answer',
-#   serial_order: 5
-# })
+QuestionOption.create!({
+  question_id: 5,
+  option_text: 'Months',
+  serial_order: 3
+})
 
-# QuestionOption.create!({
-#   question_id: 6,
-#   option_text: 'Age',
-#   serial_order: 1
-# })
+QuestionOption.create!({
+  question_id: 5,
+  option_text: 'Years',
+  serial_order: 4
+})
 
-# QuestionOption.create!({
-#   question_id: 6,
-#   option_text: 'Year Born',
-#   serial_order: 2
-# })
+QuestionOption.create!({
+  question_id: 5,
+  option_text: 'No Answer',
+  serial_order: 5
+})
 
-# QuestionOption.create!({
-#   question_id: 6,
-#   option_text: 'No Answer',
-#   serial_order: 3
-# })
+QuestionOption.create!({
+  question_id: 6,
+  option_text: 'Age',
+  serial_order: 1
+})
 
-# ## CITIES
-# puts "Creating cities ..."
+QuestionOption.create!({
+  question_id: 6,
+  option_text: 'Year Born',
+  serial_order: 2
+})
 
-# City.destroy_all
+QuestionOption.create!({
+  question_id: 6,
+  option_text: 'No Answer',
+  serial_order: 3
+})
 
-# City.create!({
-#   name: 'Vancouver',
-#   province: 'BC'
-# })
+## CITIES
+puts "Creating cities ..."
 
-# City.create!({
-#   name: 'Calgary',
-#   province: 'AB'
-# })
+City.destroy_all
 
-# ## SURVEYS
-# puts "Creating surveys ..."
+City.create!({
+  name: 'Vancouver',
+  province: 'BC'
+})
 
-# Survey.destroy_all
+City.create!({
+  name: 'Calgary',
+  province: 'AB'
+})
 
-# Survey.create!({
-#   name: 'Vancouver 2019 Sheltered Survey',
-#   description: 'To report on the total number of the homeless in the city of Vancouver in 2019',
-#   user_id: 3,
-#   city_id: 1
-# })
+## SURVEYS
+puts "Creating surveys ..."
+
+Survey.destroy_all
+
+Survey.create!({
+  name: 'Vancouver 2019 Sheltered Survey',
+  description: 'To report on the total number of the homeless in the city of Vancouver in 2019',
+  user_id: 3,
+  city_id: 1
+})
 
 puts "DONE!"
