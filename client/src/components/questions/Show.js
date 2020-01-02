46e43229be6b42918bbced4137abfb5b59f8fed3
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-02 23:48:01 +0000

    Show component for questions

---
+++ client/src/components/questions/Show.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Container, CssBaseline, Typography } from '@material-ui/core';
+import Button_Next from 'components/questions/button_next';
+import Button_Prev from 'components/questions/button_next';
+
+export default function QuestionContainer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth='md'>
+      </Container>
+    </React.Fragment>
+  );
+}
