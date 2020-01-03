d4c50c3c87ff3c19083bdaa03312f16cd46babc3
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-03 23:45:24 +0000

    add button to navigate back to full survey view

---
+++ client/src/components/questions/Back_To_Survey_Button.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+
+let classNames = require("classnames");
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(2),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export default function BackToSurveyButton(props) {
+
+  const backToSurveyButtonClass = classNames("back-to-survey-button")
+  const styleClasses = useStyles();
+
+  return (
+    <div className={styleClasses.root}>
+      <Fab class={backToSurveyButtonClass} variant="extended" id="back-to-survey-button" label="go back to view the full survey" onClick={props.onClick} size="large">
+        View Survey
+        <ListAltIcon className={styleClasses.extendedIcon} />
+      </Fab>
+      {/* <input accept="???" className={classes.input} id="icon-button-next" type="???" /> */}
+    </div>
+  );
+}
