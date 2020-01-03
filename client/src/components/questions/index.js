1dfb886d156b5e99a788f26fea2cff675c29d27a
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-03 00:58:37 +0000

    index component will control question rendering

---
+++ client/src/components/questions/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Show from 'Show';
+import Back_To_Survey from 'Back_To_Survey';
+import Button_Before from 'Button_Before';
+import Button_Next from 'Button_Next';
+import Header from 'Header';
+import Question_Text from 'Question_Text';
+import Question_Options_List from 'Question_Options_List'
+
+
+
+
+export default function Question(props) {
+
+
+
+
+
+  return (<article className="question">
+
+
+  </article>
+}
