c833ce95e45937c3678c4ead8fe0bc5e77d97d4c
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-03 23:48:44 +0000

    comment out swipeable import for now

--- client/src/components/questions/Button_Before.js
+++ client/src/components/questions/Button_Before.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import { IconButton} from '@material-ui/core';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import { useSwipeable, Swipeable } from 'react-swipeable';
+// import { useSwipeable, Swipeable } from 'react-swipeable';
 
 let classNames = require("classnames");
 
--- client/src/components/questions/button_next.js
+++ client/src/components/questions/button_next.js
@@ -3,7 +3,7 @@ import React from "react";
 
 import { IconButton} from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
-import { useSwipeable, Swipeable } from 'react-swipeable';
+// import { useSwipeable, Swipeable } from 'react-swipeable';
 
 let classNames = require("classnames");
 
