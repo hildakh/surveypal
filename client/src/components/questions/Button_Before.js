52c081c9cd38f582bc7c8d4a2760782773a33701
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-03 22:53:02 +0000

    next and before (previous) question buttons

--- client/src/components/questions/Button_Before.js
+++ client/src/components/questions/Button_Before.js
@@ -1,26 +1,22 @@
 import React from "react";
-import { makeStyles } from '@bit/mui-org.material-ui.styles';
-import { Box, Container, CssBaseline, IconButton, Typography } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+
+import { IconButton} from '@material-ui/core';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import { useSwipeable, Swipeable } from 'react-swipeable';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-}));
+let classNames = require("classnames");
 
-export default function BeforeButton() {
-  const classes = useStyles();
+export default function BeforeButton(props) {
+
+  const beforeButtonClass = classNames("before-button")
 
   return (
     <div>
-      <IconButton className={classes.button} aria-label="go to previous question" component="span">
+      <IconButton class={beforeButtonClass} id="previous-question-button" label="go to previous question" onClick={props.onClick}>
         <NavigateBeforeIcon />
       </IconButton>
-      <input accept="???" className={classes.input} id="icon-button-before" type="???" />
+      {/* <input accept="???" className={classes.input} id="icon-button-next" type="???" /> */}
     </div>
   );
 }
--- client/src/components/questions/button_next.js
+++ client/src/components/questions/button_next.js
@@ -1,27 +1,22 @@
 import React from "react";
-import { makeStyles } from '@bit/mui-org.material-ui.styles';
-import { Box, Container, CssBaseline, IconButton, Typography } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+
+import { IconButton} from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { useSwipeable, Swipeable } from 'react-swipeable';
 
+let classNames = require("classnames");
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    display: 'none',
-  },
-}));
+export default function NextButton(props) {
 
-export default function NextButton() {
-  const classes = useStyles();
+  const nextButtonClass = classNames("next-button")
 
   return (
     <div>
-      <IconButton className={classes.button} aria-label="go to next question" component="span">
+      <IconButton class={nextButtonClass} id="next-question-button" label="go to next question" onClick={props.onClick}>
         <NavigateNextIcon />
       </IconButton>
-      <input accept="???" className={classes.input} id="icon-button-next" type="???" />
+      {/* <input accept="???" className={classes.input} id="icon-button-next" type="???" /> */}
     </div>
   );
 }
