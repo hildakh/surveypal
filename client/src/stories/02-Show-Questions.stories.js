85d7ac5ee464c4b0fe1a1abfc6c6b16037d92f34
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-04 20:14:45 +0000

    comment out storybook addon-actions until further testing needed

--- client/src/stories/02-Show-Questions.stories.js
+++ client/src/stories/02-Show-Questions.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+// import { action } from '@storybook/addon-actions';
 import "../index.css";
 
 import Show from '../components/questions/Show.js';
