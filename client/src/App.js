53f09d2a80fc43d3d9471d8ed0147604782e04f1
Hilda <hilda.kh.amiri@gmail.com>
2020-01-03 17:33:24 -0800

    geo function 1st phase

--- client/src/App.js
+++ client/src/App.js
@@ -2,39 +2,43 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import AppBar from './components/navbar/AppBar';
-// import SideDrawer from './components/admin-dashboard/SideDrawer';
-// import { AppBar } from '@material-ui/core';
+
+
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Click the button to load data!'
+      message: 'Click the button to load data!',
+      latitude: null,
+      longitude: null,
+      userAddress: null
     }
   }
 
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-      .then((response) => {
-        // handle success
-        console.log(response.data) // The entire response from the Rails API
+  // fetchData = () => {
+  //   axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+  //     .then((response) => {
+  //       // handle success
+  //       console.log(response.data) // The entire response from the Rails API
 
-        console.log(response.data.message) // Just the message
-        this.setState({
-          message: response.data.message
-        });
-      })
-  }
+  //       console.log(response.data.message) // Just the message
+  //       this.setState({
+  //         message: response.data.message
+  //       });
+  //     })
+  // }
 
   render() {
     return (
       <AppBar />
-      // <div className="App">
-      //   <h1>{this.state.message}</h1>
-      //   <button onClick={this.fetchData} >
-      //     Fetch Data
-      //   </button>
-      // </div>
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <button onClick={this.fetchData} >
+          Fetch Data
+        </button>
+      </div>
     );
   }
 }
