a690916873d6dd8352f01b32fe5685394e68e85a
Hilda <hilda.kh.amiri@gmail.com>
2020-01-03 19:06:16 -0800

    user permission and lat/long was stored in state

---
+++ client/src/components/Geolocation.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+
+class Geolocation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      longitude: null,
+      userAddress: null
+    };
+
+    this.getLocation = this.getLocation.bind(this);
+    this.getCoordinates = this.getCoordinates.bind(this);
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.getCoordinates,
+        this.handleLocationError
+      );
+    } else {
+      alert("Bummer!");
+    }
+  }
+
+  handleLocationError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+      default:
+        alert("An unknown error occurred.");
+    }
+  }
+  getCoordinates(position) {
+    console.log(position);
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Geo Example</h2>
+        <button onClick={this.getLocation}>Get coordinates</button>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+        <h4>Google Maps Reverse Geocoding</h4>
+        <p>Address: {this.state.userAddress}</p>
+      </div>
+    );
+  }
+}
+
+export default Geolocation;
