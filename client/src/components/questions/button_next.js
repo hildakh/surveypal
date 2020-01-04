2767ce73c48ef40ed3a0f4a55cce6cc0420486c7
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-04 20:10:37 +0000

    remove CssBaseline import

--- client/src/components/questions/button_next.js
+++ client/src/components/questions/button_next.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 
-import { IconButton, CssBaseline } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 // import { useSwipeable, Swipeable } from 'react-swipeable';
 
