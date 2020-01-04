2dfa33b3aceb4220a9f3aaf6223063b5d5cf8270
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-04 02:31:18 +0000

    grid for survey question view

--- client/src/components/questions/Show.js
+++ client/src/components/questions/Show.js
@@ -1,14 +1,54 @@
 import React from "react";
-import { Box, Container, CssBaseline, Typography } from '@material-ui/core';
-import Button_Next from 'components/questions/button_next';
-import Button_Prev from 'components/questions/button_next';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Container, CssBaseline, Grid, Paper, Typography } from '@material-ui/core';
+// import NextButton from './Button_Next.js';
+// import BeforeButton from './Button_Before.js';
+import BackToSurveyButton from './Back_To_Survey_Button.js';
 
-export default function QuestionContainer() {
+let classNames = require("classnames");
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grid: {
+    padding: theme.spacing(0),
+    alignItems: "center"
+  },
+  item: {
+    margin: theme.spacing(.5)
+  },
+  question: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    backgroundColor: '#95b6c9'
+  },
+  questionOptions: {
+    margin: theme.spacing(2),
+    textAlign: 'center',
+    backgroundColor: '#ff795f'
+  }
+}));
+
+export default function Show(props) {
+
+  const styleClasses = useStyles();
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth='md'>
-      </Container>
-    </React.Fragment>
+
+    <div className={styleClasses.root}>
+        <Grid container className={styleClasses.grid} direction="column">
+          <Grid item xs={6} className={styleClasses.item} >
+            <BackToSurveyButton onClick="{action('button-clicked')}" aligntems="flex-start"/>
+          </Grid>
+          <Grid item sx={6} className={styleClasses.item}>
+            <Paper className={styleClasses.question}>Question goes here and how big is this grid item? Does it keep going and going and going? When will it wrap?</Paper>
+          </Grid>
+          <Grid item sx={6} className={styleClasses.questionOptions} mb={4}>
+            <Paper className={styleClasses.question}>Question options list goes here</Paper>
+          </Grid>
+        </Grid>
+    </div>
+
   );
 }
