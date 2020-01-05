870c42a55c40ea7dd55844980d9efa12ead7536f
Hilda <hilda.kh.amiri@gmail.com>
2020-01-04 17:14:45 -0800

    react icons installed to enable the use of several icon libraries

--- client/src/components/survey/Reset.js
+++ client/src/components/survey/Reset.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-import AutorenewIcon from "@material-ui/icons/Autorenew";
+// import AutorenewIcon from "@material-ui/icons/Autorenew";
+import { FaGhost } from "react-icons/fa";
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -21,7 +22,7 @@ export default function Reset() {
       <Button
         variant="contained"
         className={classes.button}
-        startIcon={<AutorenewIcon />}
+        startIcon={<FaGhost />}
       >
         Reset
       </Button>
