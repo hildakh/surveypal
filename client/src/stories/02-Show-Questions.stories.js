0dda2528d48d4ae9ebacbe9d5183be17e66dd01d
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-05 23:02:09 +0000

    stories for question text, options list and list items

--- client/src/stories/02-Show-Questions.stories.js
+++ client/src/stories/02-Show-Questions.stories.js
@@ -5,14 +5,24 @@ import { storiesOf } from '@storybook/react';
 import "../index.css";
 
 import Show from '../components/questions/Show.js';
-// import { BeforeButton } from '../components/questions/Button_Before';
+import QuestionText from '../components/questions/Question_Text';
+import QuestionOptionsList from '../components/questions/Question_Options_List';
+import QuestionOptionsListItem from '../components/questions/Question_Options_List_Item';
 
 export default {
   title: 'Show question',
 };
 
+
+// stories for question options list items
+storiesOf('Question Options', module)
+  .add('Unselected', () => <QuestionOptionsListItem option_text='Days' serial_order="1"/>)
+  .add('Selected', () => <QuestionOptionsListItem option_text='Days' serial_order="1" selected/>)
+  .add('Clickable', () => <QuestionOptionsListItem option_text='Days' serial_order="1" onClick={action('button-clicked')}/>)
+
+
 // stories for next button module
 storiesOf('Question', module)
   .add('Question', () =>  <Show />)
-
-
+  .add('Question Text', () => <QuestionText />)
+  .add('Question Options List', () => <QuestionOptionsList />)
