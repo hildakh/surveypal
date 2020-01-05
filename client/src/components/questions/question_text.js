82b9c9e35db12c32b442d560aa17b7228eae2903
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-05 22:56:25 +0000

    question text grid item moved fr Show to component file

--- client/src/components/questions/question_text.js
+++ client/src/components/questions/question_text.js
@@ -1,2 +1,27 @@
 import React from "react";
-import { Box, Container, CssBaseline, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Paper } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  item: {
+    margin: theme.spacing(.5)
+  },
+  question: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    backgroundColor: '#95b6c9'
+  }
+}));
+
+
+export default function QuestionText(props) {
+
+  const styleClasses = useStyles();
+
+  return (
+    <Grid item sx className={styleClasses.item} mx={0}>
+    <Paper className={styleClasses.question}>Question text goes here.</Paper>
+    </Grid>
+  )
+
+}
