b52205de5279503020f385e46346f667fb123c0d
Hilda <hilda.kh.amiri@gmail.com>
2020-01-05 11:12:26 -0800

    full survey view buttons completed

--- client/src/components/survey/Reset.js
+++ client/src/components/survey/Reset.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
-// import AutorenewIcon from "@material-ui/icons/Autorenew";
 import { FaGhost } from "react-icons/fa";
 
 const useStyles = makeStyles(theme => ({
