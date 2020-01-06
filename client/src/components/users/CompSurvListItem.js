567083eab5e3ed883eeddb386634f5d212183acd
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-06 12:53:31 -0800

    add feedback button to each item in list

--- client/src/components/users/CompSurvListItem.js
+++ client/src/components/users/CompSurvListItem.js
@@ -3,6 +3,7 @@ import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import { Button } from "@material-ui/core";
 
 const useStyle = makeStyles(theme => ({
   title: {
@@ -17,6 +18,9 @@ export default function CompSurvListItem(props) {
   return (
     <ListItem>
       <ListItemText className={classes.title} primary={props.title} secondary={props.date} />
+      <Button variant="outlined" color="primary" onClick={props.onClick}>
+        Feedback
+      </Button>
       <Divider />
     </ListItem>
   );
