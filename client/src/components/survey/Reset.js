2705fffabfcc95bd7a6794721e7f0a5ecbdf7e3e
Hilda <hilda.kh.amiri@gmail.com>
2020-01-06 15:39:36 -0800

    changed survey full view to a div

--- client/src/components/survey/Reset.js
+++ client/src/components/survey/Reset.js
@@ -9,7 +9,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
--- client/src/components/survey/Survey.js
+++ client/src/components/survey/Survey.js
@@ -4,24 +4,19 @@ import Back from "./Back";
 import Reset from "./Reset";
 import Start from "./Start";
 import Resume from "./Resume";
-import Modal from "@material-ui/core/Modal";
 import QuestionPreview from "./QuestionPreview";
 
 const useStyles = makeStyles(theme => ({
-  modal: {
+  surveyview: {
     width: "97%",
     backgroundColor: "#e6ceff",
-    position: "absolute",
     margin: "0 auto",
-    display: "flex",
     padding: theme.spacing(1),
-    alignItems: "center",
-    justifyContent: "center",
-    overflowY: "scroll"
   },
   heading: {
     textAlign: "center"
-  }
+  },
+
 }));
 
 export default function SurveyForm() {
@@ -37,21 +32,9 @@ export default function SurveyForm() {
   };
 
   return (
-    <div>
-      <Modal
-        disablePortal
-        disableEnforceFocus
-        disableAutoFocus
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-        className={classes.modal}
-      >
-        <div>
-          <span>
+    <div className={classes.surveyview}>
             <Back onClick={handleClose} />
             <Reset onClick={handleClose} />
-          </span>
           <h4 className={classes.heading}>Vancouver 2019 Sheltered Survey</h4>
           <p>
             <QuestionPreview />
@@ -60,8 +43,7 @@ export default function SurveyForm() {
             <Resume />
             <Start />
           </footer>
-        </div>
-      </Modal>
+
     </div>
   );
 }
