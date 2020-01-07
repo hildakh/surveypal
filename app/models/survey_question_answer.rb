70f9b9fe372c434f3b4eee07a33c46b907ac6253
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-07 04:47:48 +0000

    migrations changed to fix persistent validation error in survey_question_answers table

--- app/models/survey_question_answer.rb
+++ app/models/survey_question_answer.rb
@@ -2,6 +2,6 @@ class SurveyQuestionAnswer < ApplicationRecord
   belongs_to :survey
   belongs_to :question
   belongs_to :user
-  belongs_to :question_option
+  # belongs_to :question_option
 
 end
--- db/migrate/20200107033820_undo_change_column_null_to_question_option_id.rb
+++
@@ -1,5 +0,0 @@
-class UndoChangeColumnNullToQuestionOptionId < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :survey_question_answers, :question_option_id, false
-  end
-end
