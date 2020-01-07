f4368ddc76390ae7966324d0967aa4c65306401b
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-07 11:05:19 -0800

    add session to successful authentication

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,21 +5,15 @@ class Api::SessionsController < ApplicationController
 
   def create
     user = User.authenticate_with_credentials(params[:email], params[:password])
-    puts user.inspect
+    session[:user_id] = user.id    
     render json: {
-      user: user
+      user: user,
+      session: session
     }
-    # if user = User.authenticate_with_credentials(params[:email]), params[:password])
-    #   session[:user_id] = user.id
-    #   redirect_to '/'
-    # else
-    #   redirect_to '/login'
-    # end
   end
 
   def destroy
     # session[:user_id] = nil
-    # redirect_to '/login'
   end
 
 end
