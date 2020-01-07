9576e12fd8c9b68f53d12fe631e5b6c9e671489d
Hilda <hilda.kh.amiri@gmail.com>
2020-01-07 12:14:06 -0800

    removed the render from show method

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,13 +1,11 @@
 class Api::TeamsController < ApplicationController
 
   def index
-    @teams = Team.all
-    render json: @teams
+    teams = Team.all
+    render json: teams
    end
 
   def show
-    @teams = Team.all
-     render json: @teams
   end
 
 end
