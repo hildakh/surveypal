b0d8e7a1ee4b67954f2b7e92cd4f0ac508912b34
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-07 10:52:31 -0800

    add a function to user model to check authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  
+  def self.authenticate_with_credentials(email, password)
+    user = where('LOWER(email) = ?', email.downcase.delete(' '))[0]
+    user && user.authenticate(password)
+end
 
 end
