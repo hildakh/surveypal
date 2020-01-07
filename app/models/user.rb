badb6c7a7eaf28541636881835170bb0229de5a8
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-07 10:59:12 -0800

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   has_many :surveys
   has_many :survey_question_answers
 
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   has_secure_password
 
   def self.authenticate_with_credentials(email, password)
