3bf5bbf62cfafbae9ad9a2efb3a93bbe4161686c
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-06 22:57:33 -0800

    delete user and admin appbar component and use appbar component for all user types

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import AppBar from './components/navbar/AppBar';
-import UserAppBar from './components/navbar/UserAppBar';
 import Card from './components/users/Card';
 import Expand from 'react-expand-animated';
 import SurveyList from './components/users/SurveyList';
@@ -44,25 +43,19 @@ class App extends Component {
     return (
 
       <div className="App">
-        {this.state.userType === 'PUBLIC' && (<AppBar />)}
-        {this.state.userType === 'USER' && (
-          <div>
-            <UserAppBar />
-            <React.Fragment>
-              <Card message={'Surveys'} counter={5} onClick={this.toggleFirst} />
-              <Expand open={this.state.surveyOpen}>
-                <SurveyList list={this.state.surveyList} />
-              </Expand>
-            </React.Fragment>
-            <React.Fragment>
-              <Card message={`Today's completed surveys`} counter={10} onClick={this.toggleSecond} />
-              <Expand open={this.state.compSurvOpen}>
-                <CompSurvList list={this.state.completedSurveyList} />
-              </Expand>
-            </React.Fragment>
-          </div>
-        )}
-
+        <AppBar userType={'USER'} />
+        <React.Fragment>
+          <Card message={'Surveys'} counter={5} onClick={this.toggleFirst} />
+          <Expand open={this.state.surveyOpen}>
+            <SurveyList list={this.state.surveyList} />
+          </Expand>
+        </React.Fragment>
+        <React.Fragment>
+          <Card message={`Today's completed surveys`} counter={10} onClick={this.toggleSecond} />
+          <Expand open={this.state.compSurvOpen}>
+            <CompSurvList list={this.state.completedSurveyList} />
+          </Expand>
+        </React.Fragment>
       </div>
     );
   }
--- client/src/components/navbar/AppBar.js
+++ client/src/components/navbar/AppBar.js
@@ -2,37 +2,299 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import Login from './Login';
+import Badge from '@material-ui/core/Badge';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import BallotIcon from '@material-ui/icons/Ballot';
+import { FaUsers } from "react-icons/fa";
+import FaceIcon from '@material-ui/icons/Face';
+import PollIcon from '@material-ui/icons/Poll';
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
   },
   title: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
       display: 'block',
     },
-  }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+
 }));
 
-export default function PrimarySearchAppBar() {
-
+export default function PrimarySearchAppBar(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const userType = props.userType;
+
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = event => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleDrawerOpen = event => {
+    setDrawerOpen(true);
+  }
+
+  const handleDrawerClose = event => {
+    setDrawerOpen(false);
+  }
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={2} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
     <div className={classes.grow}>
-      < AppBar position="static" >
+
+      {drawerOpen && (<Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={drawerOpen}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button>
+            <ListItemIcon><BallotIcon /></ListItemIcon>
+            <ListItemText primary='Surveys' />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><PollIcon /></ListItemIcon>
+            <ListItemText primary='Reports' />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><FaceIcon /></ListItemIcon>
+            <ListItemText primary='Surveyors' />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon><FaUsers /></ListItemIcon>
+            <ListItemText primary='Teams' />
+          </ListItem>
+        </List>
+      </Drawer>)}
+      <AppBar position="static">
         <Toolbar>
+          {userType === "ADMIN" && (<IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}>
+            <MenuIcon />
+          </IconButton>)}
+
           <Typography className={classes.title} variant="h6" noWrap>
             SurveyPal
           </Typography>
           <div className={classes.grow} />
-          <Login />
+          {userType !== "PUBLIC" && (<div><div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit">
+              <AccountCircle />
+            </IconButton>
+            {/* add user name */}
+            <Button variant="outlined" color="default" style={{ marginLeft: '10px', marginTop: '5px', color: 'white', borderColor: 'white' }}>
+              Logout
+           </Button>
+          </div>
+            <div className={classes.sectionMobile}>
+              <IconButton
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit">
+                <MoreIcon />
+              </IconButton>
+            </div>
+          </div>)}
         </Toolbar>
-      </AppBar >
-    </div >
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
   );
 }
