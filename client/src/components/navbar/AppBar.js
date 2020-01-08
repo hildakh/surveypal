51d73ff6e61b8d6f20ed203296a11e85fbd297f0
hildakh <h_khajehamiri@yahoo.com>
2020-01-08 18:53:50 +0000

    fetch teams exported as default function

--- client/src/components/navbar/AppBar.js
+++ client/src/components/navbar/AppBar.js
@@ -27,19 +27,12 @@ import BallotIcon from '@material-ui/icons/Ballot';
 import { FaUsers } from "react-icons/fa";
 import FaceIcon from '@material-ui/icons/Face';
 import PollIcon from '@material-ui/icons/Poll';
-import axios from 'axios';
+import fetchTeams from '../../helpers/fetchteams';
+// import axios from 'axios';
 
 const drawerWidth = 240;
 
-const fetchTeams = () => {
-  axios.get('/api/teams')
-  .then(response => {
-    console.log(response.data)
-  })
-  .catch(error => {
-    console.log('Hello! Error speaking!');
-  });
-}
+
 
 const useStyles = makeStyles(theme => ({
   grow: {
