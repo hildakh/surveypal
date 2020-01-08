6dc951412e0485b2350297ea003bd985646ba47e
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-08 04:08:48 +0000

    add survey_responses relationship to survey and user models

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,6 @@
 class Survey < ApplicationRecord
   has_many :survey_questions
+  has_many :survey_responses
   belongs_to :user
   belongs_to :city
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   belongs_to :user_type
   has_many :teams
   has_many :surveys
-  has_many :survey_question_answers
+  has_many :survey_responses
 end
