74b2986bb0ddaa758113217de23e4710922017d1
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-08 03:40:05 +0000

    add serial order column to survey_questions

--- db/migrate/20200108031825_create_survey_questions.rb
+++ db/migrate/20200108031825_create_survey_questions.rb
@@ -1,6 +1,7 @@
 class CreateSurveyQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :survey_questions do |t|
+      t.integer :serial_order
       t.references :survey, null: false, foreign_key: true
       t.references :question, null: false, foreign_key: true
 
