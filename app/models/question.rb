34a128b6ecfa68aff704673335c82a2cd4e647d8
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-08 03:03:29 +0000

    validate description field and add question_type to questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   has_many :question_options
   belongs_to :question, optional: true
+  belongs_to :question_type
+
+  validates :description, presence: true
 end
