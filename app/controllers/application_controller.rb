eaa61c00e8a86bfa886e18a06394394ee2e37f13
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-09 18:14:59 +0000

    create current_use helper method & prevent CSRF attacks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
 
-  def current_user
-  end
+  # Prevent CSRF attacks by raising an exception. (with: :exception)
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :null_session
 
-  # helper_method :current_user
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
 
 end
