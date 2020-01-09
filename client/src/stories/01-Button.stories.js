2f0cc0f7c168b0e56f4e2dbc1964f4e3942b32e8
Hilda <hilda.kh.amiri@gmail.com>
2020-01-08 16:23:39 -0800

    submit button added

--- client/src/stories/01-Button.stories.js
+++ client/src/stories/01-Button.stories.js
@@ -8,6 +8,7 @@ import NextButton from '../components/questions/Button_Next.js';
 import BeforeButton from '../components/questions/Button_Before.js';
 import BackToSurveyButton from '../components/questions/Back_To_Survey_Button';
 // import { BeforeButton } from '../components/questions/Button_Before';
+import SubmitButton from '../components/questions/Submit_Button';
 
 export default {
   title: 'Message Nav Button',
@@ -15,7 +16,7 @@ export default {
 
 // stories for next button module
 storiesOf('Message Nav Buttons', module)
-  // .addParameters({ 
+  // .addParameters({
   //   backgrounds: [{ name: "white", value: "#fff", default: true }]
   // })
   .add('Next', () => <NextButton onClick={action('button-clicked')}/>)
@@ -25,4 +26,5 @@ storiesOf('Message Nav Buttons', module)
 storiesOf('Back To Survey Button')
   .add('Back to Survey', () =>  <BackToSurveyButton onClick={action('button-clicked')}/>)
 
-
+  storiesOf('Submit Button')
+  .add('Submit', () =>  <SubmitButton onClick={action('Survey Submitted!')}/>)
