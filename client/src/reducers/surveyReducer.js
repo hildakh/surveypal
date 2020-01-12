10799ff25cb4e1c795691638f77df217dc33f94f
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-12 20:13:36 +0000

    reducer sets current survey and first question, updates state with next question

--- client/src/reducers/surveyReducer.js
+++ client/src/reducers/surveyReducer.js
@@ -1,67 +1,61 @@
-const SET_SURVEY_DATA = "SET_SURVEY_DATA";
-const SET_CURRENT_SURVEY = "SET_CURRENT_SURVEY";
-const SET_SURVEY_QUESTIONS = "SET_SURVEY_QUESTIONS";
-const SET_CURRENT_QUESTION = "SET_CURRENT_QUESTION";
-const SET_CURRENT_QUESTION_OPTIONS = "SET_CURRENT_QUESTION_OPTIONS";
-const SET_CURRENT_QUESTION_RESPONSES = "SET_CURRENT_QUESTION_RESPONSES";
-const SET_QUESTION_OPTIONS = "SET_QUESTION_OPTIONS";
+const SET_SURVEY = "SET_SURVEY";
+const SET_NEXT_QUESTION = "SET_NEXT_QUESTION";
+const SET_QUESTION_RESPONSE = "SET_QUESTION_RESPONSE";
 const SET_QUESTION_RESPONSES = "SET_QUESTION_RESPONSES";
 
 export { 
-  SET_SURVEY_DATA,
-  SET_CURRENT_SURVEY,
-  SET_SURVEY_QUESTIONS,
-  SET_CURRENT_QUESTION,
-  SET_CURRENT_QUESTION_OPTIONS,
-  SET_CURRENT_QUESTION_RESPONSES,
-  SET_QUESTION_OPTIONS,
+  SET_SURVEY,
+  SET_NEXT_QUESTION,
+  SET_QUESTION_RESPONSE,
   SET_QUESTION_RESPONSES
 };
 
 export default function reducer(state, action) {
-  // set state with all app data from api calls
-  // initial state after getting data from api calls
-  if (action.type === SET_SURVEY_DATA) {
+  // initalize the survey by setting the survey passed in 
+  // from app => index => useSurveyData
+  if (action.type === SET_SURVEY) {
+    
+    console.log(action.value)
+    const first_question = action.value.questions.find(question => question.id === 1)
     return ({
       ...state,
-      current_survey: {},
-      survey_questions: {},
-      current_question: {},
-      question_options: {},
-      question_responses: {}
+      current_survey: action.value,
+      current_question: first_question
     });
   };
 
-  // update which survey is currently selected
-  if (action.type === SET_CURRENT_SURVEY) {
-    return ({ ...state, current_survey: action.value });
+  if (action.type === SET_NEXT_QUESTION) {
+    return({
+      ...state,
+      current_question: action.value
+    })
   };
+
+
+
+  ///////************/////////
+
+
   // update survey questions for currently selected survey
-  if (action.type === SET_SURVEY_QUESTIONS) {
-    return ({ ...state, survey_questions = action.value})
-  };
-  if (action.type === SET_CURRENT_QUESTION) {
-    return ({ ...state, current_question: action.value})
-  };
-  if (action.type === SET_CURRENT_QUESTION_OPTIONS) {
-    return ({ ...state, current_question: action.value})
-  };
-  if (action.type === SET_CURRENT_QUESTION_RESPONSES) {
+  // if (action.type === SET_SURVEY_QUESTIONS) {
+  //   return ({ ...state, survey_questions = action.value})
+  // };
+  // if (action.type === SET_CURRENT_QUESTION) {
+  //   return ({ ...state, current_question: action.value})
+  // };
+  // if (action.type === SET_CURRENT_QUESTION_OPTIONS) {
+  //   return ({ ...state, current_question: action.value})
+  // };
+  // if (action.type === SET_CURRENT_QUESTION_RESPONSES) {
 
-    // question response needs survey_response_id, question_option_id, and response_value
-    // survey_response_id will be added when the responses are submitted to the database
-    // const question_option_response = { question_option_id: action.value.question_option_id, 
-    //                                    response_value: action.value.response_value }
-    // const updated_responses = state.current_question_responses.push(question_option_response)
+  //   // question response needs survey_response_id, question_option_id, and response_value
+  //   // survey_response_id will be added when the responses are submitted to the database
+  //   // const question_option_response = { question_option_id: action.value.question_option_id, 
+  //   //                                    response_value: action.value.response_value }
+  //   // const updated_responses = state.current_question_responses.push(question_option_response)
 
-    return ({ ...state, current_question_responses: updated_responses})
-  };
-  if (action.type === SET_QUESTION_OPTIONS) {
-    return ({ ...state, question_options: action.value})
-  };
-  if (action.type === SET_QUESTION_RESPONSES) {
-    return ({ ...state, question_responses: action.value})
-  };
+  //   return ({ ...state, current_question_responses: updated_responses})
+  // };
 
   throw new Error(
     `tried to reduce with unsupported action type: ${action.type}`
