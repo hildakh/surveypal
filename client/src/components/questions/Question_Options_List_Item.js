09d26f14f7bea0b2a187612d50c81627209451b5
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-13 18:52:00 +0000

    remove unused import (list item secondary action)

--- client/src/components/questions/Question_Options_List_Item.js
+++ client/src/components/questions/Question_Options_List_Item.js
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Paper, List } from '@material-ui/core';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
