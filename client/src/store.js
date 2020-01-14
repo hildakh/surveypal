583ca26f30ea59f9363efbce16c8ded7e0ed9143
Vanessa Kroeker <vkroeker@gmail.com>
2020-01-14 18:45:36 +0000

    put store on the shelf for now - set up useContext within useSurveyData hook

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,13 @@
   
-import React from 'react';
+// import React from 'react';
 
-const checkedContext = React.createContext({});
+// const checkedContext = React.createContext({});
 
-// useContext hook to access state
-export const useContext = useContext(checkedContext);
+// // useContext hook to access state
+// export const useContext = useContext(checkedContext);
 
-//provides checked context to all of its children
-export const Provider = checkedContext.Provider;
-//consumes a context
-export const Consumer = checkedContext.Consumer;
-export default checkedContext;
+// //provides checked context to all of its children
+// export const Provider = checkedContext.Provider;
+// //consumes a context
+// export const Consumer = checkedContext.Consumer;
+// export default checkedContext;
