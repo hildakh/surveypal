9b30814b06b27c750a43eaa6d6ec4884cdaff92b
Baharghadimii <ghadimi.bahareh2703@gmail.com>
2020-01-15 12:12:29 -0800

    change color in view

--- client/src/components/survey/Back.js
+++ client/src/components/survey/Back.js
@@ -7,9 +7,9 @@ const useStyles = makeStyles(theme => ({
 
   button: {
     margin: theme.spacing(1),
-    background: "#849e00",
-    borderColor: "white",
-    color: "white",
+    background: "#e6af3a",
+    borderColor: "black",
+    color: "black",
     fontSize: 20,
     fontSize: '1rem',
     borderRadius: 5,
